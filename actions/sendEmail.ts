8c004d948c5839131be72e0f0c4c600dee54a480
pamis-costa <pamelapereira321@gmail.com>
2026-02-03 03:21:51 -0300

    feat: att do email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,9 +27,9 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "niyeden250@gmail.com",
+      to: "pamelapereira321@gmail.com",
       subject: "Message from contact form",
-      reply_to: senderEmail,
+      replyTo: senderEmail,
       react: React.createElement(ContactFormEmail, {
         message: message,
         senderEmail: senderEmail,
