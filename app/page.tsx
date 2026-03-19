ba1e2fed322bdc3ae80381472a5e151b1373aaee
pamis-costa <pamelapereira321@gmail.com>
2026-03-19 19:57:11 -0300

    feat: tentando configurar o analytics

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
